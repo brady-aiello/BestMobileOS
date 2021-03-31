787a745355fdf09d2f3916109fdaf34c00201420
Brady Aiello <brady@touchlab.co>
2021-03-31 13:07:11 -0700

    Update gradle version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,7 +48,7 @@ kotlin {
 }
 
 android {
-    compileSdkVersion(29)
+    compileSdkVersion(30)
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
     defaultConfig {
         minSdkVersion(24)
